9c07e4ab9270910017d54e837dfea6de0437b92d
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 22:27:26 -0600

    Update 04_unit_tests.sql

--- 04_unit_tests.sql
+++ 04_unit_tests.sql
@@ -7,7 +7,6 @@ BEGIN
   ELSE
     RAISE EXCEPTION 'Fallo: nombre incorrecto para id 1';
   END IF;
-
   IF EXISTS (
     SELECT 1 FROM usuarios WHERE id = 1 AND data->>'activo' = 'true'
   ) THEN
@@ -15,13 +14,36 @@ BEGIN
   ELSE
     RAISE EXCEPTION 'Fallo: usuario no está activo para id 1';
   END IF;
-
   IF EXISTS (
     SELECT 1 FROM usuarios WHERE id = 2 AND data->>'edad' = '25'
   ) THEN
     RAISE NOTICE 'OK: edad correcta para id 2';
   ELSE
     RAISE EXCEPTION 'Fallo: edad incorrecta para id 2';
+  END IF;
+----------------------------------------------------------------------------------
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 1 AND especificaciones->>'color' = 'Verde'
+  ) THEN
+    RAISE NOTICE 'OK: color correcto para id 1';
+  ELSE
+    RAISE EXCEPTION 'Fallo: color incorrecto para id 1';
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 1 AND especificaciones->>'Tamaño' = 'Pequeño'
+  ) THEN
+    RAISE NOTICE 'OK: objeto pequeño para id 1';
+  ELSE
+    RAISE EXCEPTION 'Fallo: objeto no es pequeño para id 1';
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 2 AND especificaciones->>'Categoría' = 'Comida'
+  ) THEN
+    RAISE NOTICE 'OK: categoría correcta para id 2';
+  ELSE
+    RAISE EXCEPTION 'Fallo: categoría incorrecta para id 2';
   END IF;
 END;
 $$;
